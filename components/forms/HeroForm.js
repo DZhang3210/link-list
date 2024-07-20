ed5e5ac5bf083b23af9eec84bd1a1ffe720214d4
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-20 15:02:30 -0400

    first commit

--- components/forms/HeroForm.js
+++ components/forms/HeroForm.js
@@ -1,11 +1,28 @@
-import React from 'react'
+'use client';
 
-const HeroForm = () => {
+import {signIn} from "next-auth/react";
+import {redirect, useRouter} from "next/navigation";
+import {useEffect} from "react";
+
+export default function HeroForm({user}) {
+  const router = useRouter();
   return (
-    <div>
-      
-    </div>
-  )
+    <form
+      // onSubmit={handleSubmit}
+      className="inline-flex items-center shadow-lg bg-white shadow-gray-500/20">
+          <span className="bg-white py-4 pl-4">
+            linklist.to/
+          </span>
+      <input
+        type="text"
+        className=""
+        style={{backgroundColor:'white',marginBottom:0,paddingLeft:0}}
+        placeholder="username"/>
+      <button
+        type="submit"
+        className="bg-blue-500 text-white py-4 px-6 whitespace-nowrap">
+        Join for Free
+      </button>
+    </form>
+  );
 }
-
-export default HeroForm
